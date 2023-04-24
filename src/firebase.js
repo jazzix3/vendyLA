7f178b0973d47b2cf9f6f7d49018ba49b1a8ad6f
Jrge-V <jorgeverduzco91@gmail.com>
2023-04-23 17:51:41 -0700

    added simple sign in using google auth

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import {getAuth } from 'firebase/auth';
 
 const firebaseConfig =({
-    apiKey: "AIzaSyDI03QBupHJvGDZCOalEGCNVtVjDRhoEAs",
-    authDomain: "vendyla-384123.firebaseapp.com",
-    projectId: "vendyla-384123",
-    storageBucket: "vendyla-384123.appspot.com",
-    messagingSenderId: "671191756543",
-    appId: "1:671191756543:web:5523891079d16e40a266ca"
+    apiKey: "AIzaSyDIBvUuJWTCExHsd4WcMbWGL-mjCvi-Y-A",
+    authDomain: "vendyla-1266c.firebaseapp.com",
+    projectId: "vendyla-1266c",
+    storageBucket: "vendyla-1266c.appspot.com",
+    messagingSenderId: "607802886951",
+    appId: "1:607802886951:web:ef02379e346562290ef702"
 })
 
 const app = initializeApp(firebaseConfig);
