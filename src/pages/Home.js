53babffdbb3b37966e6307282f40014a09bb0cbf
Jasmine Maquindang <jazzix3@gmail.com>
2023-04-27 08:51:32 -0700

    map api fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,19 @@
 import React from "react";
 import TopNav from "../components/Navbar";
-import { useLoadScript } from '@react-google-maps/api';
 import Map from "../components/Map";
 
 
 const Home = () => {
-    const { isLoaded } = useLoadScript({
-        googleMapApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-      })
-
 
     return (
-
         <>  
         <TopNav />
-        
-        <div className="container" id="main-content">
-            <div id="map-container">
-                <Map isLoaded={ isLoaded } />
+        <div className="container " id="main-content">
+            <div className="mt-5" id="map-container">
+                <Map />
             </div>
-
         </div>
-        
         </>
-        
-
-
     )
 }
 
