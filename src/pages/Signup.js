b81042bcd831759a0206a8dd74637ff416b80500
Jrge-V <jorgeverduzco91@gmail.com>
2023-04-27 05:35:20 -0700

    made it so it gathered info via google sign in, made header return first/last/profile pic instead of email

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import TopNav from "../components/Navbar";
 import { auth, db } from "../firebase";
-import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, signOut} from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
+import GoogleSignIn from "./GoogleSignUp";
 
 const Signup = () => {
     const [inputFirstName, setFirstName] = useState("");
@@ -30,14 +31,14 @@ const Signup = () => {
                         website: "",
                     }
                 })
-            .then(() => {
-                return true; // return a promise that resolves to true
-            });
+                    .then(() => {
+                        return true; // return a promise that resolves to true
+                    });
             })
             .then((docCreated) => {
                 if (docCreated) {
                     return signOut(auth).then(() => {
-                    navigate("/Login?message=New account created successfully!");
+                        navigate("/Login?message=New account created successfully!");
                     });
                 }
             })
@@ -45,43 +46,47 @@ const Signup = () => {
                 console.log(error);
                 setError("Email is already in use");
             });
-        };
+    };
 
     return (
         <>
-        <TopNav />
+            <TopNav />
 
-        <div className="container" id="main-content">
-            {error && <Alert variant="danger">{error}</Alert>}
-            <h1>Sign Up</h1>
-            <p> Already have an account?<span><Link to="/Login">Log in</Link></span></p>
+            <div className="container" id="main-content">
+                {error && <Alert variant="danger">{error}</Alert>}
+                <h1>Sign Up</h1>
+                <p> Already have an account?<span><Link to="/Login">Log in</Link></span></p>
 
-            <Form onSubmit={signup}>
-                <Form.Group className="mb-3" controlId="firstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" value={inputFirstName}  onChange={(e) => setFirstName(e.target.value)} required />
-                </Form.Group>
+                <Form onSubmit={signup}>
+                    <Form.Group className="mb-3" controlId="firstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="text" value={inputFirstName} onChange={(e) => setFirstName(e.target.value)} required />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="lastName">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" value={inputLastName} onChange={(e) => setLastName(e.target.value)} required/>
-                </Form.Group>
+                    <Form.Group className="mb-3" controlId="lastName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="text" value={inputLastName} onChange={(e) => setLastName(e.target.value)} required />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" value={inputEmail} onChange={(e) => setEmail(e.target.value)} required/>
-                </Form.Group>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" value={inputEmail} onChange={(e) => setEmail(e.target.value)} required />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" value={inputPassword} onChange={(e) => setPassword(e.target.value)} required />
-                </Form.Group>
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={inputPassword} onChange={(e) => setPassword(e.target.value)} required />
+                    </Form.Group>
 
-                <Button variant="primary" type="submit">Submit</Button>
-            </Form>
-        </div>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+
+                <div>
+                    <GoogleSignIn />
+                </div>
+            </div>
         </>
-  );
+    );
 };
 
 export default Signup;
