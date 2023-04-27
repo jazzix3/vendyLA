7feac275a28b0ab052ce4e7700a399ab29bc30d3
Jrge-V <jorgeverduzco91@gmail.com>
2023-04-27 07:04:17 -0700

    allowed users to add a pfp now (optional)

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import TopNav from "../components/Navbar";
-import { auth, db } from "../firebase";
-import { createUserWithEmailAndPassword, signOut} from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
 import GoogleSignIn from "./GoogleSignUp";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 
 const Signup = () => {
     const [inputFirstName, setFirstName] = useState("");
@@ -13,27 +14,51 @@ const Signup = () => {
     const [inputEmail, setEmail] = useState("");
     const [inputPassword, setPassword] = useState("");
     const [error, setError] = useState("");
+    const defaultUserProfile = "https://firebasestorage.googleapis.com/v0/b/myvendylaproject.appspot.com/o/Default%20Profile%20Image%2FDefault%20Pic.jpg?alt=media&token=9d04c4eb-2ba0-4f28-9e6b-f6db89acc939"
+    const [inputUserProfile, setInputUserProfile] = useState(defaultUserProfile);
     const navigate = useNavigate();
+
+    const handleUserProfileChange = (e) => {
+        setInputUserProfile(e.target.files[0]);
+    };
 
     const signup = (e) => {
         e.preventDefault();
         createUserWithEmailAndPassword(auth, inputEmail, inputPassword)
             .then((userCredential) => {
                 const user = userCredential.user;
-                return setDoc(doc(db, "users", user.uid), {
-                    firstName: inputFirstName,
-                    lastName: inputLastName,
-                    email: inputEmail,
-                    business: {
-                        businessName: "",
-                        address: "",
-                        phone: "",
-                        website: "",
-                    }
-                })
-                    .then(() => {
+                const userRef = doc(db, "users", user.uid);
+    
+                let userProfilePromise = Promise.resolve(null);
+                if (inputUserProfile && inputUserProfile !== defaultUserProfile) {
+                    const userProfileRef = ref(storage, `Profile Images/${user.uid}`);
+                    userProfilePromise = uploadBytes(userProfileRef, inputUserProfile)
+                        .then(() => {
+                            return getDownloadURL(userProfileRef);
+                        });
+                } else {
+                    userProfilePromise = Promise.resolve(defaultUserProfile);
+                }
+                
+                                
+    
+                // set user data after profile image uploaded
+                return userProfilePromise.then((userProfile) => {
+                    return setDoc(userRef, {
+                        firstName: inputFirstName,
+                        lastName: inputLastName,
+                        email: inputEmail,
+                        userProfile: userProfile,
+                        business: {
+                            businessName: "",
+                            address: "",
+                            phone: "",
+                            website: "",
+                        }
+                    }).then(() => {
                         return true; // return a promise that resolves to true
                     });
+                });
             })
             .then((docCreated) => {
                 if (docCreated) {
@@ -47,6 +72,7 @@ const Signup = () => {
                 setError("Email is already in use");
             });
     };
+    
 
     return (
         <>
@@ -77,6 +103,12 @@ const Signup = () => {
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" value={inputPassword} onChange={(e) => setPassword(e.target.value)} required />
                     </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="userProfile">
+                        <Form.Label>Profile Image</Form.Label>
+                        <Form.Control type="file" onChange={handleUserProfileChange} accept="image/*" />
+                    </Form.Group>
+
 
                     <Button variant="primary" type="submit">Submit</Button>
                 </Form>
