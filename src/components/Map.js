998217b0e0137d513ae696d4a3c681baf36c0ed8
Jasmine Maquindang <jazzix3@gmail.com>
2023-04-27 08:51:56 -0700

    map api fix

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,50 +1,50 @@
-import { GoogleMap, Marker } from "@react-google-maps/api";
-import React from "react";
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
+const containerStyle = {
+    width: '90vw',
+    height: '75vh' 
+};
 
-const Map = (props) => {
-    const { isLoaded } = props;
-    const containerStyle = {
-        width: '90vw',
-        height: '75vh' 
-    };
+const center = {
+    lat: 34.052235, 
+    lng: -118.243683 
+};
 
-    const center = {
-        lat: 34.052235, 
-        lng: -118.243683 
-    };
+const options ={
+    streetViewControl: false,
+    fullscreenControl: false,
+    mapTypeControl: false,
+    styles: [
+        {
+          featureType: "poi",
+          stylers: [{ visibility: "off" }]
+        }
+    ]
+};
 
-    const options ={
-        streetViewControl: false,
-        fullscreenControl: false,
-        mapTypeControl: false,
-        styles: [
-            {
-              featureType: "poi",
-              stylers: [{ visibility: "off" }]
-            }
-        ]
-    };
+function Map() {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY
+    })
 
     const testMarker = {
         lat: 34.2407,
         lng: -118.5300
     };
 
-
-     return isLoaded && (
+    return isLoaded ? (
         <GoogleMap
-            mapContainerStyle = {containerStyle}
-            center = {center}
-            zoom={11}
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10.5}
             options={options}
-            mapid= 'a2d9d8333221dcb3'
-            >
-        
-            <Marker position = {testMarker} />
-           
+        >
+        <Marker position = {testMarker} />
+        <></>
         </GoogleMap>
-     )
-};
+    ) : <></>
+    }
 
-export default Map;
+export default Map
