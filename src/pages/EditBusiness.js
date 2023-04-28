284c52cd511efe737809905c1ca17e6b54e81242
Jasmine Maquindang <jazzix3@gmail.com>
2023-04-28 09:48:19 -0700

    edit pages autopopulate existing info + cancel btn

--- src/pages/EditBusiness.js
+++ src/pages/EditBusiness.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TopNav from '../components/Navbar';
 import { Form, Button, Alert } from "react-bootstrap";
 import { useParams, useNavigate } from "react-router-dom";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import PlacesAutoComplete from "../components/PlacesAutoComplete";
 
@@ -14,34 +14,49 @@ const EditBusiness = () => {
     const [error, setError] = useState("");
 
     const [inputBusinessName, setBusinessName] = useState("");
-
     const [selectedLocation, setSelectedLocation] = useState(null);
-
     const [inputPhone, setPhone] = useState("");
     const [phoneIsValid, setPhoneIsValid] = useState(true);
 
+    useEffect(() => {
+        // Get the user's existing business info from Firestore
+        const getBusinessInfo = async () => {
+        try {
+            const userDocRef = doc(db, "users", userId);
+            const userDocSnap = await getDoc(userDocRef);
+
+            if (userDocSnap.exists()) {
+                const data = userDocSnap.data();
+                setBusinessName(data.business.businessName);
+                setPhone(data.business.phone);
+            }
+        } catch (error) {
+            console.log(error);
+        }};
+    getBusinessInfo();}, [userId]);
+  
+    
 
     const saveBusinessInfo = (e) => {
         e.preventDefault();
         setFormSubmitted(true);
-  
-    if (phoneIsValid) {
-        const userDocRef = doc(db, "users", userId);
-        updateDoc(userDocRef, {
-            "business.businessName": inputBusinessName,
-            "business.phone": inputPhone,
-            "business.location": selectedLocation
-        }).then(() => {
-            navigate("/Dashboard");
-        }).catch((error) => {
-            console.log(error);
-        });
-    } else {
-        setError("Invalid input. Unable to save changes.");
-    }
-  };
-  
-    // test to ensure phone number input is correct format
+
+        if (phoneIsValid) {
+            const userDocRef = doc(db, "users", userId);
+            updateDoc(userDocRef, {
+                "business.businessName": inputBusinessName,
+                "business.phone": inputPhone,
+                "business.location": selectedLocation
+            }).then(() => {
+                navigate("/Dashboard");
+            }).catch((error) => {
+                console.log(error);
+            });
+        } else {
+            setError("Invalid input. Unable to save changes.");
+        }
+    };
+
     const checkPhone = (e) => {
         const PHONE_REGEX = /^\d{3}-\d{3}-\d{4}$/; // regular expression for XXX-XXX-XXXX
         const value = e.target.value;
@@ -55,33 +70,38 @@ const EditBusiness = () => {
         <div className="container" id="main-content">
             {error && <Alert variant="danger">{error}</Alert>}
             <h1>Edit Business Information</h1>
-        
+
             <Form onSubmit={saveBusinessInfo}>
-                <Form.Group className="mb-3" controlId="BusinessName">
-                    <Form.Label>Business Name</Form.Label>
-                    <Form.Control type="text" value={inputBusinessName} onChange={(e) => setBusinessName(e.target.value)} required />
-                </Form.Group>
+            <Form.Group className="mb-3" controlId="BusinessName">
+                <Form.Label>Business Name</Form.Label>
+                <Form.Control type="text" value={inputBusinessName}
+                    onChange={(e) => setBusinessName(e.target.value)}
+                    required
+                />
+            </Form.Group>
 
-                <Form.Group className="mb-3 location" controlId="Location">
-                    <Form.Label>Location</Form.Label>
-                    <PlacesAutoComplete setLocation={setSelectedLocation} />
-                </Form.Group>
+            <Form.Group className="mb-3 location" controlId="Location">
+                <Form.Label>Location</Form.Label>
+                <PlacesAutoComplete setLocation={setSelectedLocation} />
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="Phone">
-                    <Form.Label>Phone</Form.Label>
-                    <Form.Control type="text" value={inputPhone} onChange={checkPhone} required />
-                    {!phoneIsValid && formSubmitted && (
-                        <Form.Text className="text-danger">
-                            Please enter a valid phone number in the format XXX-XXX-XXXX.
-                        </Form.Text>
-                    )}
-                </Form.Group>
-          
-                <Button variant="primary" type="submit">Save Business Information</Button>
-            </Form>
-        </div>
-        </>
-    );
+                    <Form.Group className="mb-3" controlId="Phone">
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control type="text" value={inputPhone} 
+                            onChange={checkPhone} required />
+                            {!phoneIsValid && formSubmitted && (
+                                <Form.Text className="text-danger">
+                                    Please enter a valid phone number in the format XXX-XXX-XXXX.
+                                </Form.Text>
+                            )}
+                    </Form.Group>
+            
+                    <Button variant="outline-primary" type="submit">Save Business Information</Button>
+                    <Button variant="outline-secondary" onClick={() => navigate(-1)} style={{marginLeft:"10px"}}>Cancel</Button>
+                </Form>
+            </div>
+            </>
+        );
 };
 
 export default EditBusiness;
--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,85 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TopNav from '../components/Navbar';
 import { Form, Button } from "react-bootstrap";
 import { useParams, useNavigate } from "react-router-dom";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, getDoc } from "firebase/firestore";
 import { db, storage } from "../firebase";
 import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 
 const EditProfile = () => {
-  const { userId } = useParams();
-  const navigate = useNavigate();
+    const { userId } = useParams();
+    const navigate = useNavigate();
 
-  const [inputFirstName, setFirstName] = useState("");
-  const [inputLastName, setLastName] = useState("");
-  const [inputUserProfile, setInputUserProfile] = useState(null);
-  const [userProfile, setUserProfile] = useState("");
+    const [inputFirstName, setFirstName] = useState("");
+    const [inputLastName, setLastName] = useState("");
+    const [inputUserProfile, setInputUserProfile] = useState(null);
+    const [userProfile, setUserProfile] = useState("");
 
-  const handleUserProfileChange = (e) => {
-    setInputUserProfile(e.target.files[0]);
-  };
 
-  const saveProfile = (e) => {
-    e.preventDefault();
 
-    const userDocRef = doc(db, "users", userId);
+    useEffect(() => {
+        // Get the user's existing info from Firestore
+        const getUserInfo = async () => {
+        try {
+            const userDocRef = doc(db, "users", userId);
+            const userDocSnap = await getDoc(userDocRef);
 
-    let userProfilePromise = Promise.resolve(userProfile);
+            if (userDocSnap.exists()) {
+                const data = userDocSnap.data();
+                setFirstName(data.firstName);
+                setLastName(data.lastName);
+            }
+        } catch (error) {
+            console.log(error);
+        }};
+    getUserInfo();}, [userId]);
 
-    if (inputUserProfile) {
-      const userProfileRef = ref(storage, `Profile Images/${userId}`);
-      userProfilePromise = uploadBytes(userProfileRef, inputUserProfile)
-        .then(() => {
-          return getDownloadURL(userProfileRef);
+
+
+    const handleUserProfileChange = (e) => {
+        setInputUserProfile(e.target.files[0]);
+    };
+
+    const saveProfile = (e) => {
+        e.preventDefault();
+
+        const userDocRef = doc(db, "users", userId);
+
+        let userProfilePromise = Promise.resolve(userProfile);
+
+        if (inputUserProfile) {
+        const userProfileRef = ref(storage, `Profile Images/${userId}`);
+        userProfilePromise = uploadBytes(userProfileRef, inputUserProfile)
+            .then(() => {
+            return getDownloadURL(userProfileRef);
+            });
+        }
+
+        userProfilePromise
+        .then((newUserProfile) => {
+            setUserProfile(newUserProfile);
+            return updateDoc(userDocRef, {
+            firstName: inputFirstName,
+            lastName: inputLastName,
+            userProfile: newUserProfile,
+            });
+        }).then(() => {
+            navigate("/Dashboard");
+        }).catch((error) => {
+            console.log(error);
         });
-    }
+    };
 
-    userProfilePromise
-      .then((newUserProfile) => {
-        setUserProfile(newUserProfile);
-        return updateDoc(userDocRef, {
-          firstName: inputFirstName,
-          lastName: inputLastName,
-          userProfile: newUserProfile,
-        });
-      })
-      .then(() => {
-        navigate("/Dashboard");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    return (
+        <>
+        <TopNav />
+        <div className="container" id="main-content">
+            <h1>Edit Profile</h1>
 
+            <Form onSubmit={saveProfile}>
+            <Form.Group className="mb-3" controlId="FirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" value={inputFirstName}
+                    onChange={(e) => setFirstName(e.target.value)} required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="LastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="text" value={inputLastName}
+                    onChange={(e) => setLastName(e.target.value)} required />
+            </Form.Group>
 
-  return (
-    <>
-      <TopNav />
-      <div className="container" id="main-content">
-        <h1>Edit Profile</h1>
+            <Form.Group className="mb-3" controlId="userProfile">
+                <Form.Label>Profile Image</Form.Label>
+                <Form.Control type="file" onChange={handleUserProfileChange} accept="image/*" />
+            </Form.Group>
 
-        <Form onSubmit={saveProfile}>
-          <Form.Group className="mb-3" controlId="FirstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" value={inputFirstName} onChange={(e) => setFirstName(e.target.value)} required />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="LastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" value={inputLastName} onChange={(e) => setLastName(e.target.value)} required />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="userProfile">
-            <Form.Label>Profile Image</Form.Label>
-            <Form.Control type="file" onChange={handleUserProfileChange} accept="image/*" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Save Profile
-          </Button>
-        </Form>
-      </div>
-    </>
-  );
+            <Button variant="outline-primary" type="submit">Save Profile</Button>
+            <Button variant="outline-secondary" onClick={() => navigate(-1)} style={{marginLeft:"10px"}}>Cancel</Button>
+            </Form>
+        </div>
+        </>
+    );
 };
 
 export default EditProfile;
